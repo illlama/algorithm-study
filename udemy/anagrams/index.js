cf575e7df58080c8d20b0f3240c8049224d1c413
illlama <beatlemaniakih@gmail.com>
2021-06-30 18:37:22 +0900

    anagrams answer

--- udemy/anagrams/index.js
+++ udemy/anagrams/index.js
@@ -11,19 +11,38 @@ const A = 'hello';
 const B = 'llohe';
 
 const toStructure = (text) => {
-  let refinedText = text.replace(/[^\w]/g, '').toLowerCase().split('').sort();
-  let structure = {};
-  refinedText.map((c) => {
-    structure[c] = structure[c] + 1 || 1;
-  });
-  return structure;
+  return text.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 };
+
 function anagrams(stringA, stringB) {
-  return (
-    Object.entries(toStructure(stringA)).toString() ===
-    Object.entries(toStructure(stringB)).toString()
-  );
+  return toStructure(stringA) === toStructure(stringB);
 }
+// function anagrams(stringA, stringB) {
+//   return (
+//     Object.entries(toStructure(stringA)).toString() ===
+//     Object.entries(toStructure(stringB)).toString()
+//   );
+// }
 console.log(anagrams(A, B));
 
 module.exports = anagrams;
+
+// const toStructure = (text) => {
+//     let refinedText = text.replace(/[^\w]/g, '').toLowerCase().split('').sort();
+//     let structure = {};
+//     refinedText.map((c) => {
+//       structure[c] = structure[c] + 1 || 1;
+//     });
+//     return structure;
+//   };
+
+//   function anagrams(stringA, stringB) {
+//     const aMap = toStructure(stringA);
+//     const bMap = toStructure(stringB);
+
+//     if (Object.keys(aMap).length !== Object.keys(bMap).length) return false;
+//     for (let i in aMap) {
+//       if (aMap[i] !== bMap[i]) return false;
+//     }
+//     return true;
+//   }
